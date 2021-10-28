853fdc96fa3f39c9db083d918b35b44561d5b9e8
ClareSegrue <cesegrue@gmail.com>
2021-10-28 01:20:16 -0400

    Update SettingsForm.Designer.cs

--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -40,12 +40,12 @@ namespace ProjectEcho
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.blueyellowToggle = new ProjectEcho.SwitchButton();
+            this.redgreenToggle = new ProjectEcho.SwitchButton();
+            this.invertedToggle = new ProjectEcho.SwitchButton();
+            this.grayscaleToggle = new ProjectEcho.SwitchButton();
             this.boldnessToggle = new ProjectEcho.SwitchButton();
             this.switchButton1 = new ProjectEcho.SwitchButton();
-            this.grayscaleToggle = new ProjectEcho.SwitchButton();
-            this.invertedToggle = new ProjectEcho.SwitchButton();
-            this.redgreenToggle = new ProjectEcho.SwitchButton();
-            this.blueyellowToggle = new ProjectEcho.SwitchButton();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.textsizeSelect)).BeginInit();
             this.SuspendLayout();
@@ -59,7 +59,6 @@ namespace ProjectEcho
             this.panel1.Controls.Add(this.grayscaleToggle);
             this.panel1.Controls.Add(this.boldnessToggle);
             this.panel1.Controls.Add(this.switchButton1);
-            this.panel1.Controls.Add(this.SETTINGS);
             this.panel1.Controls.Add(this.textsizeSelect);
             this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.label7);
@@ -69,27 +68,27 @@ namespace ProjectEcho
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(16, 1);
+            this.panel1.Location = new System.Drawing.Point(16, 32);
             this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(386, 421);
+            this.panel1.Size = new System.Drawing.Size(386, 329);
             this.panel1.TabIndex = 0;
             // 
             // SETTINGS
             // 
             this.SETTINGS.AutoSize = true;
-            this.SETTINGS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SETTINGS.Location = new System.Drawing.Point(143, 8);
+            this.SETTINGS.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SETTINGS.Location = new System.Drawing.Point(24, 9);
             this.SETTINGS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.SETTINGS.Name = "SETTINGS";
-            this.SETTINGS.Size = new System.Drawing.Size(143, 29);
+            this.SETTINGS.Size = new System.Drawing.Size(76, 19);
             this.SETTINGS.TabIndex = 1;
             this.SETTINGS.Text = "SETTINGS";
             // 
             // textsizeSelect
             // 
             this.textsizeSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textsizeSelect.Location = new System.Drawing.Point(80, 57);
+            this.textsizeSelect.Location = new System.Drawing.Point(283, 16);
             this.textsizeSelect.Maximum = new decimal(new int[] {
             20,
             0,
@@ -101,7 +100,7 @@ namespace ProjectEcho
             0,
             0});
             this.textsizeSelect.Name = "textsizeSelect";
-            this.textsizeSelect.Size = new System.Drawing.Size(85, 31);
+            this.textsizeSelect.Size = new System.Drawing.Size(73, 23);
             this.textsizeSelect.TabIndex = 21;
             this.textsizeSelect.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textsizeSelect.Value = new decimal(new int[] {
@@ -114,86 +113,126 @@ namespace ProjectEcho
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(179, 332);
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(25, 169);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(119, 26);
+            this.label8.Size = new System.Drawing.Size(91, 20);
             this.label8.TabIndex = 18;
-            this.label8.Text = "Red-Green";
+            this.label8.Text = "RED-GREEN";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(180, 370);
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(25, 282);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(129, 26);
+            this.label7.Size = new System.Drawing.Size(106, 20);
             this.label7.TabIndex = 16;
-            this.label7.Text = "Blue-Yellow";
+            this.label7.Text = "BLUE-YELLOW";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(179, 291);
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(25, 132);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(90, 26);
+            this.label6.Size = new System.Drawing.Size(77, 20);
             this.label6.TabIndex = 15;
-            this.label6.Text = "Inverted";
+            this.label6.Text = "INVERTED";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(180, 254);
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 243);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(120, 26);
+            this.label5.Size = new System.Drawing.Size(98, 20);
             this.label5.TabIndex = 13;
-            this.label5.Text = "Gray Scale";
+            this.label5.Text = "GRAY SCALE";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(179, 217);
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 206);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(118, 26);
+            this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 11;
-            this.label4.Text = "Dark Mode";
+            this.label4.Text = "DARK MODE";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(179, 97);
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 45);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 26);
+            this.label3.Size = new System.Drawing.Size(82, 20);
             this.label3.TabIndex = 9;
-            this.label3.Text = "Boldness";
+            this.label3.Text = "BOLDNESS";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(99, 164);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 97);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(208, 26);
+            this.label2.Size = new System.Drawing.Size(158, 18);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Color Adjustments";
+            this.label2.Text = "COLOR ADJUSTMENT";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(179, 57);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 26);
+            this.label1.Size = new System.Drawing.Size(67, 20);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Text Size";
+            this.label1.Text = "TEXT SIZE";
+            // 
+            // blueyellowToggle
+            // 
+            this.blueyellowToggle.Location = new System.Drawing.Point(304, 278);
+            this.blueyellowToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.blueyellowToggle.Name = "blueyellowToggle";
+            this.blueyellowToggle.Size = new System.Drawing.Size(52, 22);
+            this.blueyellowToggle.TabIndex = 27;
+            this.blueyellowToggle.UseVisualStyleBackColor = true;
+            this.blueyellowToggle.CheckedChanged += new System.EventHandler(this.blueyellowToggle_CheckedChanged);
+            // 
+            // redgreenToggle
+            // 
+            this.redgreenToggle.Location = new System.Drawing.Point(304, 165);
+            this.redgreenToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.redgreenToggle.Name = "redgreenToggle";
+            this.redgreenToggle.Size = new System.Drawing.Size(52, 22);
+            this.redgreenToggle.TabIndex = 26;
+            this.redgreenToggle.UseVisualStyleBackColor = true;
+            this.redgreenToggle.CheckedChanged += new System.EventHandler(this.redgreenToggle_CheckedChanged);
+            // 
+            // invertedToggle
+            // 
+            this.invertedToggle.Location = new System.Drawing.Point(304, 128);
+            this.invertedToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.invertedToggle.Name = "invertedToggle";
+            this.invertedToggle.Size = new System.Drawing.Size(52, 22);
+            this.invertedToggle.TabIndex = 25;
+            this.invertedToggle.UseVisualStyleBackColor = true;
+            this.invertedToggle.CheckedChanged += new System.EventHandler(this.invertedToggle_CheckedChanged);
+            // 
+            // grayscaleToggle
+            // 
+            this.grayscaleToggle.Location = new System.Drawing.Point(304, 239);
+            this.grayscaleToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.grayscaleToggle.Name = "grayscaleToggle";
+            this.grayscaleToggle.Size = new System.Drawing.Size(52, 22);
+            this.grayscaleToggle.TabIndex = 24;
+            this.grayscaleToggle.UseVisualStyleBackColor = true;
+            this.grayscaleToggle.CheckedChanged += new System.EventHandler(this.grayscaleToggle_CheckedChanged);
             // 
             // boldnessToggle
             // 
-            this.boldnessToggle.Location = new System.Drawing.Point(104, 97);
+            this.boldnessToggle.Location = new System.Drawing.Point(304, 55);
             this.boldnessToggle.MinimumSize = new System.Drawing.Size(45, 22);
             this.boldnessToggle.Name = "boldnessToggle";
             this.boldnessToggle.Size = new System.Drawing.Size(52, 22);
@@ -203,7 +242,7 @@ namespace ProjectEcho
             // 
             // switchButton1
             // 
-            this.switchButton1.Location = new System.Drawing.Point(104, 217);
+            this.switchButton1.Location = new System.Drawing.Point(304, 202);
             this.switchButton1.MinimumSize = new System.Drawing.Size(45, 22);
             this.switchButton1.Name = "switchButton1";
             this.switchButton1.Size = new System.Drawing.Size(52, 22);
@@ -211,52 +250,13 @@ namespace ProjectEcho
             this.switchButton1.UseVisualStyleBackColor = true;
             this.switchButton1.CheckedChanged += new System.EventHandler(this.switchButton1_CheckedChanged);
             // 
-            // grayscaleToggle
-            // 
-            this.grayscaleToggle.Location = new System.Drawing.Point(104, 254);
-            this.grayscaleToggle.MinimumSize = new System.Drawing.Size(45, 22);
-            this.grayscaleToggle.Name = "grayscaleToggle";
-            this.grayscaleToggle.Size = new System.Drawing.Size(52, 22);
-            this.grayscaleToggle.TabIndex = 24;
-            this.grayscaleToggle.UseVisualStyleBackColor = true;
-            this.grayscaleToggle.CheckedChanged += new System.EventHandler(this.grayscaleToggle_CheckedChanged);
-            // 
-            // invertedToggle
-            // 
-            this.invertedToggle.Location = new System.Drawing.Point(104, 291);
-            this.invertedToggle.MinimumSize = new System.Drawing.Size(45, 22);
-            this.invertedToggle.Name = "invertedToggle";
-            this.invertedToggle.Size = new System.Drawing.Size(52, 22);
-            this.invertedToggle.TabIndex = 25;
-            this.invertedToggle.UseVisualStyleBackColor = true;
-            this.invertedToggle.CheckedChanged += new System.EventHandler(this.invertedToggle_CheckedChanged);
-            // 
-            // redgreenToggle
-            // 
-            this.redgreenToggle.Location = new System.Drawing.Point(104, 332);
-            this.redgreenToggle.MinimumSize = new System.Drawing.Size(45, 22);
-            this.redgreenToggle.Name = "redgreenToggle";
-            this.redgreenToggle.Size = new System.Drawing.Size(52, 22);
-            this.redgreenToggle.TabIndex = 26;
-            this.redgreenToggle.UseVisualStyleBackColor = true;
-            this.redgreenToggle.CheckedChanged += new System.EventHandler(this.redgreenToggle_CheckedChanged);
-            // 
-            // blueyellowToggle
-            // 
-            this.blueyellowToggle.Location = new System.Drawing.Point(104, 370);
-            this.blueyellowToggle.MinimumSize = new System.Drawing.Size(45, 22);
-            this.blueyellowToggle.Name = "blueyellowToggle";
-            this.blueyellowToggle.Size = new System.Drawing.Size(52, 22);
-            this.blueyellowToggle.TabIndex = 27;
-            this.blueyellowToggle.UseVisualStyleBackColor = true;
-            this.blueyellowToggle.CheckedChanged += new System.EventHandler(this.blueyellowToggle_CheckedChanged);
-            // 
             // SettingsForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(423, 428);
+            this.ClientSize = new System.Drawing.Size(423, 372);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.SETTINGS);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SettingsForm";
@@ -267,6 +267,7 @@ namespace ProjectEcho
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.textsizeSelect)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
