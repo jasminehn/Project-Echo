7f0a3c9b9f5eae441ec5fb1ebbbddfe51a331c0d
SentientBagel <jhnelson.contact@gmail.com>
2021-10-28 01:02:25 -0400

    updated settings menu
    pls work

--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -32,20 +32,20 @@ namespace ProjectEcho
             this.panel1 = new System.Windows.Forms.Panel();
             this.SETTINGS = new System.Windows.Forms.Label();
             this.textsizeSelect = new System.Windows.Forms.NumericUpDown();
-            this.boldSwitch = new System.Windows.Forms.Button();
-            this.redgreenSwitch = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
-            this.blueyellowSwitch = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.grayscaleSwitch = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
-            this.invertedSwitch = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.darkModeSwitch = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.boldnessToggle = new ProjectEcho.SwitchButton();
+            this.switchButton1 = new ProjectEcho.SwitchButton();
+            this.grayscaleToggle = new ProjectEcho.SwitchButton();
+            this.invertedToggle = new ProjectEcho.SwitchButton();
+            this.redgreenToggle = new ProjectEcho.SwitchButton();
+            this.blueyellowToggle = new ProjectEcho.SwitchButton();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.textsizeSelect)).BeginInit();
             this.SuspendLayout();
@@ -53,19 +53,19 @@ namespace ProjectEcho
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.blueyellowToggle);
+            this.panel1.Controls.Add(this.redgreenToggle);
+            this.panel1.Controls.Add(this.invertedToggle);
+            this.panel1.Controls.Add(this.grayscaleToggle);
+            this.panel1.Controls.Add(this.boldnessToggle);
+            this.panel1.Controls.Add(this.switchButton1);
             this.panel1.Controls.Add(this.SETTINGS);
             this.panel1.Controls.Add(this.textsizeSelect);
-            this.panel1.Controls.Add(this.boldSwitch);
-            this.panel1.Controls.Add(this.redgreenSwitch);
             this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.blueyellowSwitch);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.grayscaleSwitch);
             this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.invertedSwitch);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.darkModeSwitch);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
@@ -82,14 +82,14 @@ namespace ProjectEcho
             this.SETTINGS.Location = new System.Drawing.Point(143, 8);
             this.SETTINGS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.SETTINGS.Name = "SETTINGS";
-            this.SETTINGS.Size = new System.Drawing.Size(97, 20);
+            this.SETTINGS.Size = new System.Drawing.Size(143, 29);
             this.SETTINGS.TabIndex = 1;
             this.SETTINGS.Text = "SETTINGS";
             // 
             // textsizeSelect
             // 
             this.textsizeSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textsizeSelect.Location = new System.Drawing.Point(35, 172);
+            this.textsizeSelect.Location = new System.Drawing.Point(80, 57);
             this.textsizeSelect.Maximum = new decimal(new int[] {
             20,
             0,
@@ -101,7 +101,7 @@ namespace ProjectEcho
             0,
             0});
             this.textsizeSelect.Name = "textsizeSelect";
-            this.textsizeSelect.Size = new System.Drawing.Size(85, 23);
+            this.textsizeSelect.Size = new System.Drawing.Size(85, 31);
             this.textsizeSelect.TabIndex = 21;
             this.textsizeSelect.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textsizeSelect.Value = new decimal(new int[] {
@@ -111,56 +111,23 @@ namespace ProjectEcho
             0});
             this.textsizeSelect.ValueChanged += new System.EventHandler(this.textsizeSelect_SelectedItemChanged);
             // 
-            // boldSwitch
-            // 
-            this.boldSwitch.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.boldSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.boldSwitch.Location = new System.Drawing.Point(213, 163);
-            this.boldSwitch.Name = "boldSwitch";
-            this.boldSwitch.Size = new System.Drawing.Size(50, 30);
-            this.boldSwitch.TabIndex = 20;
-            this.boldSwitch.UseVisualStyleBackColor = false;
-            this.boldSwitch.Click += new System.EventHandler(this.boldSwitch_Click);
-            // 
-            // redgreenSwitch
-            // 
-            this.redgreenSwitch.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.redgreenSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.redgreenSwitch.Location = new System.Drawing.Point(29, 356);
-            this.redgreenSwitch.Name = "redgreenSwitch";
-            this.redgreenSwitch.Size = new System.Drawing.Size(50, 30);
-            this.redgreenSwitch.TabIndex = 19;
-            this.redgreenSwitch.UseVisualStyleBackColor = false;
-            this.redgreenSwitch.Click += new System.EventHandler(this.redgreenSwitch_Click);
-            // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(85, 359);
+            this.label8.Location = new System.Drawing.Point(179, 332);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(81, 18);
+            this.label8.Size = new System.Drawing.Size(119, 26);
             this.label8.TabIndex = 18;
             this.label8.Text = "Red-Green";
-            // 
-            // blueyellowSwitch
-            // 
-            this.blueyellowSwitch.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.blueyellowSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.blueyellowSwitch.Location = new System.Drawing.Point(214, 356);
-            this.blueyellowSwitch.Name = "blueyellowSwitch";
-            this.blueyellowSwitch.Size = new System.Drawing.Size(50, 30);
-            this.blueyellowSwitch.TabIndex = 17;
-            this.blueyellowSwitch.UseVisualStyleBackColor = false;
-            this.blueyellowSwitch.Click += new System.EventHandler(this.blueyellowSwitch_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(270, 359);
+            this.label7.Location = new System.Drawing.Point(180, 370);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(85, 18);
+            this.label7.Size = new System.Drawing.Size(129, 26);
             this.label7.TabIndex = 16;
             this.label7.Text = "Blue-Yellow";
             // 
@@ -168,82 +135,49 @@ namespace ProjectEcho
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(85, 296);
+            this.label6.Location = new System.Drawing.Point(179, 291);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(59, 18);
+            this.label6.Size = new System.Drawing.Size(90, 26);
             this.label6.TabIndex = 15;
             this.label6.Text = "Inverted";
-            // 
-            // grayscaleSwitch
-            // 
-            this.grayscaleSwitch.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.grayscaleSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.grayscaleSwitch.Location = new System.Drawing.Point(214, 293);
-            this.grayscaleSwitch.Name = "grayscaleSwitch";
-            this.grayscaleSwitch.Size = new System.Drawing.Size(50, 30);
-            this.grayscaleSwitch.TabIndex = 14;
-            this.grayscaleSwitch.UseVisualStyleBackColor = false;
-            this.grayscaleSwitch.Click += new System.EventHandler(this.grayscaleSwitch_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(270, 296);
+            this.label5.Location = new System.Drawing.Point(180, 254);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 18);
+            this.label5.Size = new System.Drawing.Size(120, 26);
             this.label5.TabIndex = 13;
             this.label5.Text = "Gray Scale";
-            // 
-            // invertedSwitch
-            // 
-            this.invertedSwitch.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.invertedSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.invertedSwitch.Location = new System.Drawing.Point(29, 293);
-            this.invertedSwitch.Name = "invertedSwitch";
-            this.invertedSwitch.Size = new System.Drawing.Size(50, 30);
-            this.invertedSwitch.TabIndex = 12;
-            this.invertedSwitch.UseVisualStyleBackColor = false;
-            this.invertedSwitch.Click += new System.EventHandler(this.invertedSwitch_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(85, 74);
+            this.label4.Location = new System.Drawing.Point(179, 217);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 18);
+            this.label4.Size = new System.Drawing.Size(118, 26);
             this.label4.TabIndex = 11;
             this.label4.Text = "Dark Mode";
-            // 
-            // darkModeSwitch
-            // 
-            this.darkModeSwitch.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.darkModeSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.darkModeSwitch.Location = new System.Drawing.Point(32, 71);
-            this.darkModeSwitch.Name = "darkModeSwitch";
-            this.darkModeSwitch.Size = new System.Drawing.Size(50, 30);
-            this.darkModeSwitch.TabIndex = 10;
-            this.darkModeSwitch.UseVisualStyleBackColor = false;
-            this.darkModeSwitch.Click += new System.EventHandler(this.switchButton_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(269, 169);
+            this.label3.Location = new System.Drawing.Point(179, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 18);
+            this.label3.Size = new System.Drawing.Size(102, 26);
             this.label3.TabIndex = 9;
             this.label3.Text = "Boldness";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 254);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 164);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 18);
+            this.label2.Size = new System.Drawing.Size(208, 26);
             this.label2.TabIndex = 4;
             this.label2.Text = "Color Adjustments";
             // 
@@ -251,11 +185,71 @@ namespace ProjectEcho
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 145);
+            this.label1.Location = new System.Drawing.Point(179, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.Size = new System.Drawing.Size(102, 26);
             this.label1.TabIndex = 3;
             this.label1.Text = "Text Size";
+            // 
+            // boldnessToggle
+            // 
+            this.boldnessToggle.Location = new System.Drawing.Point(104, 97);
+            this.boldnessToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.boldnessToggle.Name = "boldnessToggle";
+            this.boldnessToggle.Size = new System.Drawing.Size(52, 22);
+            this.boldnessToggle.TabIndex = 23;
+            this.boldnessToggle.UseVisualStyleBackColor = true;
+            this.boldnessToggle.CheckedChanged += new System.EventHandler(this.boldnessToggle_CheckedChanged);
+            // 
+            // switchButton1
+            // 
+            this.switchButton1.Location = new System.Drawing.Point(104, 217);
+            this.switchButton1.MinimumSize = new System.Drawing.Size(45, 22);
+            this.switchButton1.Name = "switchButton1";
+            this.switchButton1.Size = new System.Drawing.Size(52, 22);
+            this.switchButton1.TabIndex = 22;
+            this.switchButton1.UseVisualStyleBackColor = true;
+            this.switchButton1.CheckedChanged += new System.EventHandler(this.switchButton1_CheckedChanged);
+            // 
+            // grayscaleToggle
+            // 
+            this.grayscaleToggle.Location = new System.Drawing.Point(104, 254);
+            this.grayscaleToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.grayscaleToggle.Name = "grayscaleToggle";
+            this.grayscaleToggle.Size = new System.Drawing.Size(52, 22);
+            this.grayscaleToggle.TabIndex = 24;
+            this.grayscaleToggle.UseVisualStyleBackColor = true;
+            this.grayscaleToggle.CheckedChanged += new System.EventHandler(this.grayscaleToggle_CheckedChanged);
+            // 
+            // invertedToggle
+            // 
+            this.invertedToggle.Location = new System.Drawing.Point(104, 291);
+            this.invertedToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.invertedToggle.Name = "invertedToggle";
+            this.invertedToggle.Size = new System.Drawing.Size(52, 22);
+            this.invertedToggle.TabIndex = 25;
+            this.invertedToggle.UseVisualStyleBackColor = true;
+            this.invertedToggle.CheckedChanged += new System.EventHandler(this.invertedToggle_CheckedChanged);
+            // 
+            // redgreenToggle
+            // 
+            this.redgreenToggle.Location = new System.Drawing.Point(104, 332);
+            this.redgreenToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.redgreenToggle.Name = "redgreenToggle";
+            this.redgreenToggle.Size = new System.Drawing.Size(52, 22);
+            this.redgreenToggle.TabIndex = 26;
+            this.redgreenToggle.UseVisualStyleBackColor = true;
+            this.redgreenToggle.CheckedChanged += new System.EventHandler(this.redgreenToggle_CheckedChanged);
+            // 
+            // blueyellowToggle
+            // 
+            this.blueyellowToggle.Location = new System.Drawing.Point(104, 370);
+            this.blueyellowToggle.MinimumSize = new System.Drawing.Size(45, 22);
+            this.blueyellowToggle.Name = "blueyellowToggle";
+            this.blueyellowToggle.Size = new System.Drawing.Size(52, 22);
+            this.blueyellowToggle.TabIndex = 27;
+            this.blueyellowToggle.UseVisualStyleBackColor = true;
+            this.blueyellowToggle.CheckedChanged += new System.EventHandler(this.blueyellowToggle_CheckedChanged);
             // 
             // SettingsForm
             // 
@@ -281,19 +275,19 @@ namespace ProjectEcho
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label SETTINGS;
         private System.Windows.Forms.NumericUpDown textsizeSelect;
-        private System.Windows.Forms.Button boldSwitch;
-        private System.Windows.Forms.Button redgreenSwitch;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button blueyellowSwitch;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button grayscaleSwitch;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button invertedSwitch;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button darkModeSwitch;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private SwitchButton switchButton1;
+        private SwitchButton boldnessToggle;
+        private SwitchButton grayscaleToggle;
+        private SwitchButton invertedToggle;
+        private SwitchButton redgreenToggle;
+        private SwitchButton blueyellowToggle;
     }
 }
--- SettingsForm.cs
+++ SettingsForm.cs
@@ -14,18 +14,37 @@ namespace ProjectEcho
     {
         bool on = true;
 
-        private void switchOnOff(Button button)
+        //button colors
+        Color buttonOnColor = ColorTranslator.FromHtml("#5ac993");
+        Color buttonOffColor = SystemColors.ControlDark;
+
+        //light mode colors
+        Color lightBG = Color.WhiteSmoke;
+        Color lightText = Color.Black;
+
+        //dark mode colors
+        Color darkBG = ColorTranslator.FromHtml("#121113");
+        Color darkText = Color.WhiteSmoke;
+        
+
+        private void switchOnOff(SwitchButton button)
         {
             if (on)
             {
-                button.Text = "ON";
-                button.BackColor = Color.LimeGreen;
+                //button.Text = "ON";
+                //switchButton1.Checked = true;
+                //button.BackColor = Color.LimeGreen;
+
+                //button.BackColor = buttonOnColor;
                 on = false;
             }
             else
             {
-                button.Text = "OFF";
-                button.BackColor = Color.DarkGray;
+                //button.Text = "OFF";
+                //switchButton1.Checked = false;
+                //button.BackColor = Color.DarkGray;
+
+                //button.BackColor = buttonOffColor;
                 on = true;
             }
         }
@@ -37,38 +56,55 @@ namespace ProjectEcho
 
         private void SettingsForm_Load(object sender, EventArgs e)
         {
-            darkModeSwitch.Text = "OFF";
+            /*darkModeSwitch.Text = "OFF";
             invertedSwitch.Text = "OFF";
             grayscaleSwitch.Text = "OFF";
             blueyellowSwitch.Text = "OFF";
             redgreenSwitch.Text = "OFF";
-            boldSwitch.Text = "OFF";
+            boldSwitch.Text = "OFF";*/
             textsizeSelect.Value = (decimal)label1.Font.Size;
         }
 
         private void switchButton_Click(object sender, EventArgs e)
         {
-            switchOnOff(darkModeSwitch);
+            //switchOnOff(darkModeSwitch);
             if (on)
             {
+                /*
                 BackColor = System.Drawing.Color.White;
                 textsizeSelect.BackColor = Color.White;
+                textsizeSelect.ForeColor = Color.Black;
                 ForeColor = System.Drawing.Color.Black;
+                */
+                BackColor = lightBG;
+                panel1.BackColor = lightBG;
+                ForeColor = Color.Black; //changes all text color
+                textsizeSelect.BackColor = Color.White;
+                textsizeSelect.ForeColor = Color.Black;
+
                 on = true;
             }
-            else
+            else //DARK MODE ON
             {
+                /*
                 BackColor = System.Drawing.Color.Black;
                 textsizeSelect.BackColor = Color.Black;
                 textsizeSelect.ForeColor = Color.White;
                 ForeColor = System.Drawing.Color.White;
+                */
+                BackColor = darkBG;
+                panel1.BackColor = darkBG;
+                ForeColor = Color.White; //changes all text color
+                textsizeSelect.BackColor = Color.Black;
+                textsizeSelect.ForeColor = Color.White;
+                
                 on = false;
             }
         }
 
         private void invertedSwitch_Click(object sender, EventArgs e)
         {
-            switchOnOff(invertedSwitch);
+            //switchOnOff(invertedSwitch);
             if (on)
             {
                 //normal
@@ -83,7 +119,7 @@ namespace ProjectEcho
 
         private void grayscaleSwitch_Click(object sender, EventArgs e)
         {
-            switchOnOff(grayscaleSwitch);
+            //switchOnOff(grayscaleSwitch);
             if (on)
             {
                 //normal
@@ -98,7 +134,7 @@ namespace ProjectEcho
 
         private void blueyellowSwitch_Click(object sender, EventArgs e)
         {
-            switchOnOff(blueyellowSwitch);
+            //switchOnOff(blueyellowSwitch);
             if (on)
             {
                 //normal
@@ -113,7 +149,7 @@ namespace ProjectEcho
 
         private void redgreenSwitch_Click(object sender, EventArgs e)
         {
-            switchOnOff(redgreenSwitch);
+            //switchOnOff(redgreenSwitch);
             if (on)
             {
                 //normal
@@ -128,7 +164,7 @@ namespace ProjectEcho
         
         private void boldSwitch_Click(object sender, EventArgs e)
         {
-            switchOnOff(boldSwitch);
+            //switchOnOff(boldSwitch);
             var controls = getAll(this, typeof(Label));
             if (on)
             {
@@ -171,5 +207,128 @@ namespace ProjectEcho
             return controls.SelectMany(ctrl => getAll(ctrl, type)).Concat(controls).Where(c => c.GetType() == type);
         }
 
+
+        //NEW CODE WITH TOGGLES
+
+        private void switchButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            switchOnOff(switchButton1);
+            if (on)
+            {
+                /*
+                BackColor = System.Drawing.Color.White;
+                textsizeSelect.BackColor = Color.White;
+                textsizeSelect.ForeColor = Color.Black;
+                ForeColor = System.Drawing.Color.Black;
+                */
+                BackColor = lightBG;
+                panel1.BackColor = lightBG;
+                ForeColor = Color.Black; //changes all text color
+                textsizeSelect.BackColor = Color.White;
+                textsizeSelect.ForeColor = Color.Black;
+
+                on = true;
+            }
+            else //DARK MODE ON
+            {
+                /*
+                BackColor = System.Drawing.Color.Black;
+                textsizeSelect.BackColor = Color.Black;
+                textsizeSelect.ForeColor = Color.White;
+                ForeColor = System.Drawing.Color.White;
+                */
+                BackColor = darkBG;
+                panel1.BackColor = darkBG;
+                ForeColor = Color.White; //changes all text color
+                textsizeSelect.BackColor = Color.Black;
+                textsizeSelect.ForeColor = Color.White;
+
+                on = false;
+            }
+        }
+
+        private void boldnessToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            switchOnOff(boldnessToggle);
+            var controls = getAll(this, typeof(Label));
+            if (on)
+            {
+                Font = new System.Drawing.Font(Font, FontStyle.Regular);
+                foreach (Control c in controls)
+                {
+                    c.Font = new System.Drawing.Font(Font, FontStyle.Regular);
+                }
+                on = true;
+            }
+            else
+            {
+                Font = new System.Drawing.Font(Font, FontStyle.Bold);
+                foreach (Control c in controls)
+                {
+                    c.Font = new System.Drawing.Font(Font, FontStyle.Bold);
+                }
+                on = false;
+            }
+        }
+
+        private void grayscaleToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            switchOnOff(grayscaleToggle);
+            if (on)
+            {
+                //normal
+                on = true;
+            }
+            else
+            {
+                //greyscale
+                on = false;
+            }
+        }
+
+        private void invertedToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            switchOnOff(invertedToggle);
+            if (on)
+            {
+                //normal
+                on = true;
+            }
+            else
+            {
+                //inverted
+                on = false;
+            }
+        }
+
+        private void redgreenToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            switchOnOff(redgreenToggle);
+            if (on)
+            {
+                //normal
+                on = true;
+            }
+            else
+            {
+                //filter
+                on = false;
+            }
+        }
+
+        private void blueyellowToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            switchOnOff(blueyellowToggle);
+            if (on)
+            {
+                //normal
+                on = true;
+            }
+            else
+            {
+                //filter
+                on = false;
+            }
+        }
     }
 }
