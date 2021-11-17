aec85159e8096712a38673ef6908590c2500a2dd
Isabel Gnagy <76077843+isabel-gnagy@users.noreply.github.com>
2021-11-17 11:04:15 -0500

    comments

--- SettingsForm.cs
+++ SettingsForm.cs
@@ -40,32 +40,38 @@ namespace ProjectEcho
             boldSwitch.Text = "OFF";*/
             //textsizeSelect.Value = (decimal)textsizeLabel.Font.Size;
         }
-
+        
         private void switchButton_Click(object sender, EventArgs e)
         {
-            if (switchButton.Checked)
+            //Checks darkmode toggle
+            if (switchButton.Checked) //ON
             {                
                 BackColor = darkBG;
                 panel1.BackColor = darkBG;
                 ForeColor = Color.White; //changes all text color
+
                 //Changes color for slider
                 textsizeAdjust.BackColor = Color.Black;
                 textsizeAdjust.ForeColor = Color.White;
+
                 //Changes color for button
                 applyButton.BackColor = Color.Black;
             }
-            else //DARK MODE ON
+            else //OFF
             {
                 BackColor = lightBG;
                 panel1.BackColor = lightBG;
                 ForeColor = Color.Black; //changes all text color
+
                 //Changes color for slider
                 textsizeAdjust.BackColor = Color.White;
                 textsizeAdjust.ForeColor = Color.Black;
+
                 //Changes color for button
                 applyButton.BackColor = Color.White;
             }
         }
+
        public IEnumerable<Control> getAll(Control control, Type type)
         {
             var controls = control.Controls.Cast<Control>();
@@ -74,24 +80,27 @@ namespace ProjectEcho
 
         private void boldnessToggle_CheckedChanged(object sender, EventArgs e)
         {
-            var controls = getAll(this, typeof(Label));
-            if (boldnessToggle.Checked)
+            var controls = getAll(this, typeof(Label)); //Finds all labels
+
+            //Checks boldness toggles
+            if (boldnessToggle.Checked) //ON
             {
-                Font = new System.Drawing.Font(Font, FontStyle.Bold);
-                foreach (Control c in controls)
+                foreach (Control c in controls) //Changes every label indivdually
                 {
-                    FontFamily fam = c.Font.FontFamily;
-                    float s = c.Font.Size;
+                    FontFamily fam = c.Font.FontFamily; //Sets current font style
+                    float s = c.Font.Size; //Sets current font size
+
+                    //Passes in font style, size and changes to bold
                     c.Font = new System.Drawing.Font(fam, s, FontStyle.Bold);
                 }
             }
-            else
+            else //OFF
             {
-                Font = new System.Drawing.Font(Font, FontStyle.Regular);
                 foreach (Control c in controls)
                 {
                     FontFamily fam = c.Font.FontFamily;
                     float s = c.Font.Size;
+
                     c.Font = new System.Drawing.Font(fam, s, FontStyle.Regular);
                 }
             }
@@ -160,25 +169,27 @@ namespace ProjectEcho
 
         private void textsizeAdjust_Scroll(object sender, EventArgs e)
         {
-            var controls = getAll(this, typeof(Label));
+            var controls = getAll(this, typeof(Label)); //Finds all labels
             
             float curr = textsizeAdjust.Value; // gets current font size
 
             //Checks if the slider val is getting larger or smaller
             if (prev < curr)
             {
-                foreach (Control c in controls)
+                foreach (Control c in controls) //Changes every label indivdually
                 {
                     FontFamily fon = Font.FontFamily; //Sets font family 
                     FontStyle sty = c.Font.Style; //Sets style (ie. bold, italic, reg)
                     float adjSize = c.Font.Size + curr;
-                    c.Font = new Font(fon, adjSize, sty); //Passes in family, style, new size
+
+                    //Passes in family, style, and changes to new size
+                    c.Font = new Font(fon, adjSize, sty);
                 }
                 prev = curr; //Sets prev to current size for next interation
             }
             else
             {
-                //If the slider is set back down to 0, it subs 0. This subs 1 as it should
+                //If the slider is set back down to 0, it subtracts 1 instead of 0
                 if(curr == 0)
                 {
                     foreach (Control c in controls)
