620adab0f6357ce05063ce6dfd2de0b07a5306e0
Isabel Gnagy <76077843+isabel-gnagy@users.noreply.github.com>
2021-12-15 22:07:19 -0500

    help form

--- HelpForm.Designer.cs
+++ HelpForm.Designer.cs
@@ -58,9 +58,10 @@ namespace ProjectEcho
             "Task 1",
             "Task 2",
             "Task 3"});
-            this.comboBox1.Location = new System.Drawing.Point(924, 54);
+            this.comboBox1.Location = new System.Drawing.Point(1232, 66);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(281, 21);
+            this.comboBox1.Size = new System.Drawing.Size(373, 24);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -68,9 +69,10 @@ namespace ProjectEcho
             // 
             this.partComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.partComboBox.FormattingEnabled = true;
-            this.partComboBox.Location = new System.Drawing.Point(924, 81);
+            this.partComboBox.Location = new System.Drawing.Point(1232, 100);
+            this.partComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.partComboBox.Name = "partComboBox";
-            this.partComboBox.Size = new System.Drawing.Size(281, 21);
+            this.partComboBox.Size = new System.Drawing.Size(373, 24);
             this.partComboBox.TabIndex = 2;
             this.partComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
@@ -78,18 +80,20 @@ namespace ProjectEcho
             // 
             this.label2.BackColor = System.Drawing.Color.DarkBlue;
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Location = new System.Drawing.Point(16, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(606, 18);
+            this.label2.Size = new System.Drawing.Size(808, 22);
             this.label2.TabIndex = 3;
             this.label2.Text = "Select the Task and the Part you would like to read the instructions of in the dr" +
     "op down boxes. OR, search by keyword phrase.";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(154, 80);
+            this.textBox1.Location = new System.Drawing.Point(205, 98);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(451, 20);
+            this.textBox1.Size = new System.Drawing.Size(600, 22);
             this.textBox1.TabIndex = 4;
             this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
             // 
@@ -98,9 +102,10 @@ namespace ProjectEcho
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(853, 57);
+            this.label3.Location = new System.Drawing.Point(1137, 70);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.Size = new System.Drawing.Size(86, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Select Task:";
             // 
@@ -110,9 +115,10 @@ namespace ProjectEcho
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.DarkBlue;
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(854, 85);
+            this.label4.Location = new System.Drawing.Point(1139, 105);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.Size = new System.Drawing.Size(81, 17);
             this.label4.TabIndex = 6;
             this.label4.Text = "Select Part:";
             // 
@@ -121,9 +127,10 @@ namespace ProjectEcho
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.DarkBlue;
             this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label5.Location = new System.Drawing.Point(12, 83);
+            this.label5.Location = new System.Drawing.Point(16, 102);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.Size = new System.Drawing.Size(180, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "Search by keyword phrase:";
             // 
@@ -145,17 +152,19 @@ namespace ProjectEcho
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1340, 102);
+            this.panel1.Size = new System.Drawing.Size(1787, 126);
             this.panel1.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.DarkBlue;
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Location = new System.Drawing.Point(16, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(224, 18);
+            this.label1.Size = new System.Drawing.Size(299, 22);
             this.label1.TabIndex = 14;
             this.label1.Text = "Select the Document you would like to use:";
             // 
@@ -167,18 +176,20 @@ namespace ProjectEcho
             this.pdfComboBox.Items.AddRange(new object[] {
             "Requirements & Rubrics",
             "Making Good Choices"});
-            this.pdfComboBox.Location = new System.Drawing.Point(242, 22);
+            this.pdfComboBox.Location = new System.Drawing.Point(323, 27);
+            this.pdfComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pdfComboBox.Name = "pdfComboBox";
-            this.pdfComboBox.Size = new System.Drawing.Size(363, 21);
+            this.pdfComboBox.Size = new System.Drawing.Size(483, 24);
             this.pdfComboBox.TabIndex = 13;
             this.pdfComboBox.SelectedIndexChanged += new System.EventHandler(this.pdfComboBox_SelectedIndexChanged);
             // 
             // nextButton
             // 
             this.nextButton.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nextButton.Location = new System.Drawing.Point(665, 78);
+            this.nextButton.Location = new System.Drawing.Point(887, 96);
+            this.nextButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nextButton.Name = "nextButton";
-            this.nextButton.Size = new System.Drawing.Size(48, 22);
+            this.nextButton.Size = new System.Drawing.Size(64, 27);
             this.nextButton.TabIndex = 12;
             this.nextButton.Text = "NEXT";
             this.nextButton.UseVisualStyleBackColor = true;
@@ -187,9 +198,10 @@ namespace ProjectEcho
             // prevButton
             // 
             this.prevButton.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prevButton.Location = new System.Drawing.Point(611, 78);
+            this.prevButton.Location = new System.Drawing.Point(815, 96);
+            this.prevButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.prevButton.Name = "prevButton";
-            this.prevButton.Size = new System.Drawing.Size(48, 22);
+            this.prevButton.Size = new System.Drawing.Size(64, 27);
             this.prevButton.TabIndex = 11;
             this.prevButton.Text = "PREV";
             this.prevButton.UseVisualStyleBackColor = true;
@@ -199,9 +211,10 @@ namespace ProjectEcho
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(1211, 66);
+            this.button1.Location = new System.Drawing.Point(1615, 81);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.Size = new System.Drawing.Size(100, 27);
             this.button1.TabIndex = 8;
             this.button1.Text = "GO";
             this.button1.UseVisualStyleBackColor = true;
@@ -212,19 +225,22 @@ namespace ProjectEcho
             this.ChromiumBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ChromiumBrowser.Location = new System.Drawing.Point(7, 108);
+            this.ChromiumBrowser.Location = new System.Drawing.Point(9, 133);
+            this.ChromiumBrowser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ChromiumBrowser.Name = "ChromiumBrowser";
-            this.ChromiumBrowser.Size = new System.Drawing.Size(1320, 575);
+            this.ChromiumBrowser.Size = new System.Drawing.Size(1760, 708);
             this.ChromiumBrowser.TabIndex = 9;
+            this.ChromiumBrowser.Tag = "panelBw";
             // 
             // urlBox
             // 
             this.urlBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.urlBox.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.urlBox.Location = new System.Drawing.Point(12, 782);
+            this.urlBox.Location = new System.Drawing.Point(16, 962);
+            this.urlBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.urlBox.Name = "urlBox";
-            this.urlBox.Size = new System.Drawing.Size(1043, 20);
+            this.urlBox.Size = new System.Drawing.Size(1389, 22);
             this.urlBox.TabIndex = 10;
             // 
             // panel2
@@ -234,22 +250,24 @@ namespace ProjectEcho
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.BackColor = System.Drawing.Color.DarkBlue;
             this.panel2.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.panel2.Location = new System.Drawing.Point(-4, 99);
+            this.panel2.Location = new System.Drawing.Point(-5, 122);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1344, 594);
+            this.panel2.Size = new System.Drawing.Size(1792, 731);
             this.panel2.TabIndex = 12;
             // 
             // HelpForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1334, 690);
+            this.ClientSize = new System.Drawing.Size(1779, 849);
             this.Controls.Add(this.urlBox);
             this.Controls.Add(this.ChromiumBrowser);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(1251, 684);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(1662, 831);
             this.Name = "HelpForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Help";
